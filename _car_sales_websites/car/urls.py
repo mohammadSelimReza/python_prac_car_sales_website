068a3d44662424951c7e7ea59ef9a58012f51a02
mohammadSelimReza <srreza1999@gmail.com>
2024-07-10 09:49:34 +0600

    half-done

--- _car_sales_websites/car/urls.py
+++ _car_sales_websites/car/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.product_view,name='homePage')
+    path('', views.product_view, name='homePage'),
+    path('brand/<slug:brand_slug>/', views.product_view, name='brand_filter'),
 ]
--- _car_sales_websites/car/views.py
+++ _car_sales_websites/car/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from .models import Car
-# Create your views here.
-def product_view(request):
-    data = Car.objects.all()
-    return render(request,'products.html',{'data':data})
+from django.shortcuts import render, get_object_or_404
+from .models import Car, Brand
+
+def product_view(request, brand_slug=None):
+    if brand_slug:
+        brand = get_object_or_404(Brand, slug=brand_slug)
+        data = Car.objects.filter(brand=brand)
+    else:
+        data = Car.objects.all()
+    
+    brands = Brand.objects.all()
+    return render(request, 'products.html', {'data': data, 'brands': brands, 'brand_slug': brand_slug})
